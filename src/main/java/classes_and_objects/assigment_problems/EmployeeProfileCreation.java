a8472ea92a6ec15e9746cc5db958cbe12834ecfc
Naveen M <nnaveen20242@gmail.com>
2026-09-14 00:07:12 +0530

    Add Week 6 Employee Profile Creation assignment

---
+++ src/main/java/classes_and_objects/assigment_problems/EmployeeProfileCreation.java
@@ -0,0 +1,34 @@
+package classes_and_objects.assigment_problems;
+
+class EmployeeProfile {
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public EmployeeProfile(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public EmployeeProfile(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    public void printProfile() {
+        System.out.println(empId + " | " + empName + " | Rs " + salary + " | Intern: " + isIntern);
+    }
+}
+
+public class EmployeeProfileCreation {
+    public static void main(String[] args) {
+        EmployeeProfile permanent = new EmployeeProfile("E-101", "Divya", 65000);
+        EmployeeProfile intern = new EmployeeProfile("E-102", "Arjun");
+
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
